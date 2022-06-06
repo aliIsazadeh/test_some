eb826b629d422e58366ff7854caab5eac8deb70b
ali forghani <a.forghani@sain.ir>
2022-06-06 23:46:11 +0430

    step 1 for parametrization

--- src/main/java/MovingMomentumStrategy.java
+++ src/main/java/MovingMomentumStrategy.java
@@ -48,7 +48,16 @@ public class MovingMomentumStrategy {
      * @param series the bar series
      * @return the moving momentum strategy
      */
-    public static Strategy buildStrategy(BarSeries series) {
+    public static Strategy buildStrategy(BarSeries series,
+                                         int shortEMABarCount,
+                                         int longEMABarCount,
+                                         int stochasticOscillKBarCount,
+                                         int MACDShortBarCount,
+                                         int MACDLongBarCount,
+                                         int EMAMACDBarCount,
+                                         int entryThreshold,
+                                         int exitThreshold
+                                         ) {
         if (series == null) {
             throw new IllegalArgumentException("Series cannot be null");
         }
@@ -59,22 +68,26 @@ public class MovingMomentumStrategy {
         // moving average.
         // The bias is bearish when the shorter-moving average moves below the longer
         // moving average.
-        EMAIndicator shortEma = new EMAIndicator(closePrice, 9);
-        EMAIndicator longEma = new EMAIndicator(closePrice, 26);
+        EMAIndicator shortEma = new EMAIndicator(closePrice, shortEMABarCount);
+        EMAIndicator longEma = new EMAIndicator(closePrice, longEMABarCount);
 
-        StochasticOscillatorKIndicator stochasticOscillK = new StochasticOscillatorKIndicator(series, 14);
+        StochasticOscillatorKIndicator stochasticOscillK =
+                new StochasticOscillatorKIndicator(series, stochasticOscillKBarCount);
 
-        MACDIndicator macd = new MACDIndicator(closePrice, 9, 26);
-        EMAIndicator emaMacd = new EMAIndicator(macd, 18);
+        MACDIndicator macd = new MACDIndicator(closePrice, MACDShortBarCount,
+                MACDLongBarCount);
+        EMAIndicator emaMacd = new EMAIndicator(macd, EMAMACDBarCount);
 
         // Entry rule
         Rule entryRule = new OverIndicatorRule(shortEma, longEma) // Trend
-                .and(new CrossedDownIndicatorRule(stochasticOscillK, 20)) // Signal 1
+                .and(new CrossedDownIndicatorRule(stochasticOscillK, entryThreshold)) //
+                // Signal 1
                 .and(new OverIndicatorRule(macd, emaMacd)); // Signal 2
 
         // Exit rule
         Rule exitRule = new UnderIndicatorRule(shortEma, longEma) // Trend
-                .and(new CrossedUpIndicatorRule(stochasticOscillK, 80)) // Signal 1
+                .and(new CrossedUpIndicatorRule(stochasticOscillK, exitThreshold)) //
+                // Signal 1
                 .and(new UnderIndicatorRule(macd, emaMacd)); // Signal 2
 
         return new BaseStrategy(entryRule, exitRule);
@@ -85,14 +98,16 @@ public class MovingMomentumStrategy {
         // Getting the bar series
         BarSeries series = CsvTradesLoader.loadBitstampSeries();
 
+        long l = System.currentTimeMillis();
         // Building the trading strategy
-        Strategy strategy = buildStrategy(series);
+        Strategy strategy = buildStrategy(series,0,0,0,0,0,0
+        ,0,0);
 
         // Running the strategy
         BarSeriesManager seriesManager = new BarSeriesManager(series);
         TradingRecord tradingRecord = seriesManager.run(strategy);
         System.out.println("Number of positions for the strategy: " + tradingRecord.getPositionCount());
-
+        System.out.println(System.currentTimeMillis()-l);
         // Analysis
         System.out.println(
                 "Total profit for the strategy: " + new GrossReturnCriterion().calculate(series, tradingRecord));
